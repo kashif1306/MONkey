68608bbe2652d8dd7718d8f3fdcdc2675373a538
Kashif <kashif.chintal@gmail.com>
2026-01-20 00:03:26 +0530

    Fix CORS with correct frontend URL

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 const app = express()
 app.use(cors({
-  origin: ['https://monkey-qcfm.onrender.com', 'http://localhost:5173'],
+  origin: ['https://monkey-qx1m.onrender.com', 'http://localhost:5173'],
   credentials: true
 }))
 app.use(express.json())
